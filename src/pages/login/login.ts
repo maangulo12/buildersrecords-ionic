26126e57e1d6207e5adc87aefd8530a4ab887fd0
Miguel Angulo <maangulo12@gmail.com>
2016-12-08 16:56:43 -0600

    Update login module

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,45 +1,27 @@
 import { Component } from '@angular/core';
-import { NavController, LoadingController } from 'ionic-angular';
+import { NavController } from 'ionic-angular';
 
 import { ProjectsPage } from '../projects/projects';
 import { SignupPage } from '../signup/signup';
-import { User } from '../../models/user';
-import { UserService } from '../../providers/user.service';
 
 @Component({
   selector: 'page-login',
-  templateUrl: 'login.html',
-  providers: [UserService]
+  templateUrl: 'login.html'
 })
-export class LoginPage {
-
-  user: User;
+export class LoginPage {                                     
 
   constructor(
-    public navCtrl: NavController, 
-    public loadingCtrl: LoadingController,
-    private userService: UserService
+    public navCtrl: NavController
   ) {}
 
   logIn() {
     // Authenticate
 
-    // Present Loading 
-    this.presentLoading();
-
-    // Go to Projects page
+    // Success
     this.navCtrl.push(ProjectsPage);
   }
 
   goToSignup() {
     this.navCtrl.push(SignupPage);
-  }
-  
-  presentLoading() {
-    let loader = this.loadingCtrl.create({
-      content: "Please wait...",
-      duration: 50
-    });
-    loader.present();
   }
 }
