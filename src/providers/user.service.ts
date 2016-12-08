3f29746655a79b4e7e516408cbaae49209045959
Miguel Angulo <maangulo12@gmail.com>
2016-12-08 16:14:25 -0600

    Update user service

--- src/providers/user.service.ts
+++ src/providers/user.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
 
 import { ConstantService } from './constant.service';
 import { User } from '../models/user';
@@ -7,12 +10,44 @@ import { User } from '../models/user';
 @Injectable()
 export class UserService {
 
+    userUrl: string;
+
     constructor(
         public http: Http,
         public constantService: ConstantService
-    ) { 
-
+    ) {
+        this.userUrl = this.constantService.API_ENDPOINT + '/users';
     }
 
+    getUser(): void {}
+
+    addUser(user: User): Observable<User> {
+        let body = JSON.stringify(user);
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+
+        return this.http.post(this.userUrl, body, options)
+                        .map(this.extractData)
+                        .catch(this.handleError);
+    }
+
+    updateUser(): void {}
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || { };
+    }
     
+    private handleError(error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
 }
