372b05530380a59271a2ec74ecf6e0605be17cbf
Miguel Angulo <maangulo12@gmail.com>
2016-12-10 16:24:58 -0600

    Update overview module

--- src/pages/overview/overview.ts
+++ src/pages/overview/overview.ts
@@ -1,17 +1,21 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { JobCostPage } from '../job-cost/job-cost';
+import { SubcontractorsPage } from '../subcontractors/subcontractors';
+import { FundsPage } from '../funds/funds';
+import { ExpendituresPage } from '../expenditures/expenditures';
+
 @Component({
   selector: 'page-overview',
   templateUrl: 'overview.html'
 })
 export class OverviewPage {
 
-  tabOverview: any;
-  tabJobCost: any;
-  tabSubcontractor: any;
-  tabFunds: any;
-  tabExpenditures: any;
+  tabJobCost:       any = JobCostPage;
+  tabSubcontractor: any = SubcontractorsPage;
+  tabFunds:         any = FundsPage;
+  tabExpenditures:  any = ExpendituresPage;
 
   constructor(public navCtrl: NavController) {}
 
