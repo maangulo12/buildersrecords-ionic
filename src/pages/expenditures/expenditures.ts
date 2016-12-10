c3483e3dbaaeb85981e278f00c45c15396365147
Miguel Angulo <maangulo12@gmail.com>
2016-12-09 20:24:46 -0600

    Update expenditures module

--- src/pages/expenditures/expenditures.ts
+++ src/pages/expenditures/expenditures.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-/*
-  Generated class for the Expenditures page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-expenditures',
   templateUrl: 'expenditures.html'
 })
-export class Expenditures {
+export class ExpendituresPage {
 
   constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello Expenditures Page');
-  }
 
 }
