bc1af039559267a68e368ae8b7db37dd483a25cb
Miguel Angulo <maangulo12@gmail.com>
2016-12-09 20:24:53 -0600

    Update funds module

--- src/pages/funds/funds.ts
+++ src/pages/funds/funds.ts
@@ -1,22 +1,12 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
-/*
-  Generated class for the Funds page.
-
-  See http://ionicframework.com/docs/v2/components/#navigation for more info on
-  Ionic pages and navigation.
-*/
 @Component({
   selector: 'page-funds',
   templateUrl: 'funds.html'
 })
-export class Funds {
+export class FundsPage {
 
   constructor(public navCtrl: NavController) {}
-
-  ionViewDidLoad() {
-    console.log('Hello Funds Page');
-  }
 
 }
