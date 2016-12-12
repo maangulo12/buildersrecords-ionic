aeb1fd6cddf87c93c5dbd2861cb06d1b40904002
Miguel Angulo <maangulo12@gmail.com>
2016-12-11 23:46:19 -0600

    Update subcontractors module

--- src/pages/subcontractors/subcontractors.ts
+++ src/pages/subcontractors/subcontractors.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-subcontractors',
   templateUrl: 'subcontractors.html'
 })
-export class SubcontractorsPage {
-
-  constructor(public navCtrl: NavController) {}
-
-}
+export class SubcontractorsPage {}
