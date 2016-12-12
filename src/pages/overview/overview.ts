85f8095ea8a4c4d2a90823b2a2d4751b19da03ff
Miguel Angulo <maangulo12@gmail.com>
2016-12-11 22:00:05 -0600

    Update overview module

--- src/pages/overview/overview.ts
+++ src/pages/overview/overview.ts
@@ -1,14 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
-
-import { ProjectsPage } from '../projects/projects';
 
 @Component({
   selector: 'page-overview',
   templateUrl: 'overview.html'
 })
-export class OverviewPage {
-
-  constructor(public navCtrl: NavController) {}
-  
-}
+export class OverviewPage {}
