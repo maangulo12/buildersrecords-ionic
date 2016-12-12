052eb2e015e1128fd0b209436aa274bbaed6998b
Miguel Angulo <maangulo12@gmail.com>
2016-12-11 21:59:26 -0600

    Update job cost module

--- src/pages/job-cost/job-cost.ts
+++ src/pages/job-cost/job-cost.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-job-cost',
   templateUrl: 'job-cost.html'
 })
-export class JobCostPage {
-
-  constructor(public navCtrl: NavController) {}
-
-}
+export class JobCostPage {}
