e98406c55925a005088ddf16ee6455ac03141b6a
Miguel Angulo <maangulo12@gmail.com>
2016-12-11 23:59:07 -0600

    Update funds module

--- src/pages/funds/funds.ts
+++ src/pages/funds/funds.ts
@@ -1,12 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
 
 @Component({
   selector: 'page-funds',
   templateUrl: 'funds.html'
 })
-export class FundsPage {
-
-  constructor(public navCtrl: NavController) {}
-
-}
+export class FundsPage {}
